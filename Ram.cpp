65b947690a92c68e0062c7aa953b2b621c8cb47c
Suyeon Shin <suyeons5536@gmail.com>
2023-07-27 22:09:21 +0900

    chapter_3_12

---
+++ Ram.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Ram.h"
+using namespace std;
+
+Ram::Ram()
+{
+    for(int i=0; i<sizeof(mem)/sizeof(char); i++)
+        mem[i] = 0;
+}
+Ram::~Ram(){ cout << "메모리 제거됨" << endl; }
+char Ram::read(int address) 
+{
+    return mem[address];
+}
+void Ram::write(int address, char value)
+{
+    mem[address] = value;
+}
