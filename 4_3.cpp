e79c38d90cad2efcc16847e6dd00304808775eea
Suyeon Shin <suyeons5536@gmail.com>
2023-08-07 17:31:57 +0900

    chapter_4_3.2

--- 4_3.cpp
+++ 4_3.cpp
@@ -7,18 +7,20 @@ using namespace std;
 int main()
 {
     string text;
-    int index = 0;
+    int index = -1;
     int count = 0;
 
     cout << "문자열 입력 >> ";
     getline(cin, text);
 
-    for(int i=0; i<text.length(); i++)
-        text[i] = tolower(text[i]);
-
-    while(text.find('a', index) <= text.length() - index)
-        count ++;
-
+    while (true)
+    {
+		index = text.find('a', index+1);
+        
+		if (index == -1)
+            break;
+		else count++;
+	}
     cout << "문자 a는 " << count << "개 있습니다." << endl;
 
     return 0;
